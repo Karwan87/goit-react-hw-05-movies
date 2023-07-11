e6c82d323a4b5839877a116105dcbe74b823d4a2
Karwan87 <damianuszynski@gmail.com>
2023-07-11 20:44:08 +0200

    Update Reviews.jsx
    Reviews.jsx created - to use in MovieDetails.jsx

---
+++ src/components/Reviews/Reviews.jsx
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import axios from 'axios';
+
+const API_KEY = '4315a3747153818fe39eb54a50eb0402';
+
+function Reviews({ movieId }) {
+  const [reviews, setReviews] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.themoviedb.org/3/movie/${movieId}/reviews?api_key=${API_KEY}`
+      )
+      .then(response => {
+        setLoading(false);
+        setReviews(response.data.results);
+      })
+      .catch(error => {
+        console.error('Error fetching movie reviews:', error);
+      });
+  }, [movieId]);
+
+  return (
+    <div>
+      <h2>Movie Reviews</h2>
+      {loading ? (
+        <div>Loading...</div>
+      ) : (
+        <ul>
+          {reviews.map(review => (
+            <li key={review.id}>
+              <h3>{review.author}</h3>
+              <p>{review.content}</p>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+}
+
+Reviews.propTypes = {
+  movieId: PropTypes.string,
+};
+
+export default Reviews;
