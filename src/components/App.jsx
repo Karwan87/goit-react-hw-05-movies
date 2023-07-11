b62ddb0ad1f97a531c057a9ad5527b80dd58e89c
Karwan87 <damianuszynski@gmail.com>
2023-07-11 20:47:40 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,7 +11,7 @@ const App = () => {
     <div>
       <nav>
         <Link to="/">Home</Link>
-        <Link to="/movies">Movies</Link>
+        <Link to="/movies">Movies...</Link>
       </nav>
       <Routes>
         <Route path="/" element={<Home />} />
