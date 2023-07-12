78576a9ed3685d54875d973f2cd7cc589254499b
Karwan87 <damianuszynski@gmail.com>
2023-07-12 22:31:44 +0200

    Cast styling

--- src/components/Cast/Cast.jsx
+++ src/components/Cast/Cast.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import axios from 'axios';
+import styles from './Cast.module.css';
 
 const API_KEY = '4315a3747153818fe39eb54a50eb0402';
 
@@ -23,19 +24,29 @@ function Cast({ movieId }) {
   }, [movieId]);
 
   return (
-    <div>
-      <h2>Movie Cast</h2>
+    <div className={styles.CastContainer}>
       {loading ? (
         <div>Loading...</div>
       ) : (
         <ul>
-          {cast.map(actor => (
-            <li key={actor.id}>
-              <img
-                src={`https://image.tmdb.org/t/p/w200${actor.profile_path}`}
-                alt={actor.name}
-              />
-              <p>{actor.name}</p>
+          {cast.map((actor, index) => (
+            <li key={`${actor.id}-${index}`}>
+              {actor.profile_path ? (
+                <img
+                  src={`https://image.tmdb.org/t/p/w200${actor.profile_path}`}
+                  alt={actor.name}
+                />
+              ) : (
+                <div className={styles.NoImage}>
+                  <p className={styles.TextNoImg}>No image</p>
+                </div>
+              )}
+              <ul>
+                <li className={styles.ActorName}>{actor.name}</li>
+                <li className={styles.ActorCharacter}>
+                  Character: {actor.character}
+                </li>
+              </ul>
             </li>
           ))}
         </ul>
