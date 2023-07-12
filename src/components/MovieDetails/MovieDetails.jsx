2bded7dd7a960a8756718d8b7f169b2e14116492
Karwan87 <damianuszynski@gmail.com>
2023-07-12 22:31:24 +0200

    Styling movies & trying URL changing
    Styling movies

--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -1,11 +1,14 @@
-import React, { useEffect, useState } from 'react';
-import { useParams, Link } from 'react-router-dom';
+import React, { useEffect, useState, lazy, Suspense } from 'react';
+import { useParams, Link, useNavigate } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import axios from 'axios';
-import Cast from 'components/Cast/Cast';
-import Reviews from 'components/Reviews/Reviews';
+// import Cast from '../Cast/Cast';
+// import Reviews from '../Reviews/Reviews';
+import styles from './MovieDetails.module.css';
 
 const API_KEY = '4315a3747153818fe39eb54a50eb0402';
+const Cast = lazy(() => import('../Cast/Cast'));
+const Reviews = lazy(() => import('../Reviews/Reviews'));
 
 function MovieDetails() {
   const { movieId } = useParams();
@@ -13,6 +16,7 @@ function MovieDetails() {
   const [loading, setLoading] = useState(true);
   const [showCast, setShowCast] = useState(false);
   const [showReviews, setShowReviews] = useState(false);
+  const navigate = useNavigate();
 
   useEffect(() => {
     axios
@@ -28,9 +32,18 @@ function MovieDetails() {
 
   const handleToggleCast = () => {
     setShowCast(prevState => !prevState);
+
+    if (!showCast) {
+      setShowReviews(false);
+    }
   };
+
   const handleToggleReviews = () => {
     setShowReviews(prevState => !prevState);
+
+    if (!showReviews) {
+      setShowCast(false);
+    }
   };
 
   if (loading) {
@@ -40,25 +53,53 @@ function MovieDetails() {
 
   return (
     <div>
-      <h2>{movieDetails.title}</h2>
-      <p>{movieDetails.overview}</p>
-      <p>Release Date: {movieDetails.release_date}</p>
-      <p>Rating: {votePercentage}%</p>
-      <p>Genres: {movieDetails.genres.map(genre => genre.name).join(', ')}</p>
-      {movieDetails.poster_path && (
-        <img
-          src={`https://image.tmdb.org/t/p/w200${movieDetails.poster_path}`}
-          alt={movieDetails.title}
-        />
-      )}
-      <Link to="#" onClick={handleToggleCast}>
-        {showCast ? 'Hide Cast' : 'Show Cast'}
-      </Link>
-      {showCast && <Cast movieId={movieId} />}
-      <Link to="#" onClick={handleToggleReviews}>
-        {showReviews ? 'Hide Reviews' : 'Show Reviews'}
-      </Link>
-      {showReviews && <Reviews movieId={movieId} />}
+      <div className={styles.MovieContainer}>
+        <div className={styles.MoviePoster}>
+          {movieDetails.poster_path && (
+            <img
+              src={`https://image.tmdb.org/t/p/w200${movieDetails.poster_path}`}
+              alt={movieDetails.title}
+            />
+          )}
+        </div>
+        <div className={styles.MovieDetail}>
+          <h2 className={styles.MovieTitle}>
+            {movieDetails.title} ({movieDetails.release_date.substring(0, 4)})
+          </h2>
+          <h5>User Score: {votePercentage}%</h5>
+          <h4>Overview:</h4>
+          <h5> {movieDetails.overview}</h5>
+
+          <h4>Genres:</h4>
+          <h5> {movieDetails.genres.map(genre => genre.name).join(', ')}</h5>
+        </div>
+      </div>
+      <div className={styles.BtnContainer}>
+        <h4 className={styles.Extras}>Additional information</h4>
+        <button className={styles.CastBtn} onClick={handleToggleCast}>
+          {showCast ? 'Cast' : 'Cast'}
+        </button>
+        <button className={styles.ReviewsBtn} onClick={handleToggleReviews}>
+          {showReviews ? 'Reviews' : 'Reviews'}
+        </button>
+      </div>
+      <div>
+        {showCast && (
+          <div>
+            <Suspense fallback={<div>Loading Cast...</div>}>
+              <Cast movieId={movieId} />
+            </Suspense>
+          </div>
+        )}
+
+        {showReviews && (
+          <div>
+            <Suspense fallback={<div>Loading Reviews...</div>}>
+              <Reviews movieId={movieId} />
+            </Suspense>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
